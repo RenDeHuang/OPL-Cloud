5ed9eed4f94be4d28c6f213bbce0cb084ad3a342
Feng GAO <gaofeng21cn@hotmail.com>
2026-08-13 09:03:57 +0800

    fix: route nil-store renewal fixtures through explicit memory tables

--- services/control-plane/internal/server/workspace_renewal_test.go
+++ services/control-plane/internal/server/workspace_renewal_test.go
@@ -25,7 +25,7 @@ type workspaceRenewalAPIFixture struct {
 }
 
 func newWorkspaceRenewalAPIFixture(t *testing.T) workspaceRenewalAPIFixture {
-	return newWorkspaceRenewalAPIFixtureWithStore(t, nil)
+	return newWorkspaceRenewalAPIFixtureWithStore(t, newMemoryTableStore())
 }
 
 func newWorkspaceRenewalAPIFixtureWithStore(t *testing.T, store StateStore) workspaceRenewalAPIFixture {
@@ -131,7 +131,7 @@ func TestWorkspaceAutoRenewAllowsDisable(t *testing.T) {
 }
 
 func TestCloudAdminCanManageOwnWorkspaceRenewal(t *testing.T) {
-	server, err := NewPersistentServer(newTestService(fakeLedgerClient{}, &fakeFabricClient{}), nil)
+	server, err := NewPersistentServer(newTestService(fakeLedgerClient{}, &fakeFabricClient{}), newMemoryTableStore())
 	if err != nil {
 		t.Fatal(err)
 	}
